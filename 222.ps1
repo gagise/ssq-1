64bc420a34943a4ab0816eec94e3ff9993047525
mator2006 <45257633+mator2006@users.noreply.github.com>
2019-03-30 17:58:21 +0800

    a
    a

---
+++ 222.ps1
@@ -0,0 +1,14 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2019 v5.6.160
+	 Created on:   	2019/3/30 17:36
+	 Created by:   	sampson
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>Get-Process -Name "microsoftedge" | Stop-Process -force
+
+
